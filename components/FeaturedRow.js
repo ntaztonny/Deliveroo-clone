4fc0e6482f962e639d7c73c197887d63f3ca7707
ntaztonny <atozpp@yahoo.com>
2022-07-23 22:52:52 +0200

    added restaraunt cards and featured maps

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
+import RestaurantCards from "./RestaurantCards";
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
@@ -19,6 +20,54 @@ const FeaturedRow = ({ id, title, description }) => {
         className="pt-4"
       >
         {/* Restaurant cards */}
+        <RestaurantCards
+          id={1}
+          imgURL="https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+          title="Restarant 1"
+          rating={3.8}
+          genre="Italien"
+          address="11 rue saint pierre"
+          short_desription="French resto"
+          dishes={[]}
+          long={6457}
+          lat={263674}
+        />
+        <RestaurantCards
+          id={2}
+          imgURL="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          title="Restarant 2"
+          rating={4.4}
+          genre="French"
+          address="11 rue saint pierre"
+          short_desription="French resto"
+          dishes={[]}
+          long={6457}
+          lat={263674}
+        />
+        <RestaurantCards
+          id={3}
+          imgURL="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+          title="Restarant 3"
+          rating={4.8}
+          genre="Japanese"
+          address="11 rue saint pierre"
+          short_desription
+          dishes={[]}
+          long={6457}
+          lat={263674}
+        />
+        <RestaurantCards
+          id={4}
+          imgURL="https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          title="Restarant 4"
+          rating={4.3}
+          genre="Indian"
+          address="11 rue saint pierre"
+          short_desription="French resto"
+          dishes={[]}
+          long={6457}
+          lat={263674}
+        />
       </ScrollView>
     </View>
   );
--- components/RestaurantCards.js
+++ components/RestaurantCards.js
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
-
+import { StarIcon } from "react-native-heroicons/solid";
+import { LocationMarkerIcon } from "react-native-heroicons/outline";
 const RestaurantCards = ({
   id,
-  imURL,
+  imgURL,
   title,
   rating,
   genre,
@@ -14,9 +15,24 @@ const RestaurantCards = ({
   lat,
 }) => {
   return (
-    <View>
-      <Text>RestaurantCards</Text>
-    </View>
+    <TouchableOpacity className="bg-white mr-4 shadow">
+      <Image source={{ uri: imgURL }} className="h-36 w-64  rounded-sm" />
+      <View className="px-2 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon size={22} color="#00CCBB" opacity={0.5} />
+          <Text className="font-bold text-xs text-gray-500">
+            {rating} . {genre}
+          </Text>
+        </View>
+        <View className="flex-row items-center space-x-1 pt-1">
+          <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500 font-bold">
+            Nearby . {address}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
